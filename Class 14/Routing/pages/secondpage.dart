efd4c0e12d9e31dfa5d80da97b317558a3bd4b38
Sayeed Hasan <65469507+sayeedhasan1387@users.noreply.github.com>
2021-10-04 17:28:26 +0600

    Create secondpage.dart

---
+++ Class 14/Routing/pages/secondpage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'homepage.dart';
+import 'thirdpage.dart';
+
+class secondpage extends StatelessWidget {
+  const secondpage({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return secondpage();
+  }
+}
+
