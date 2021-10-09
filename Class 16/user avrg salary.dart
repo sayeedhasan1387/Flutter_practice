96c9c04828fb09a3daf1a3ed0ecf7d12c4a32604
Sayeed Hasan <65469507+sayeedhasan1387@users.noreply.github.com>
2021-10-09 15:41:14 +0600

    Average salary make in dart

---
+++ Class 16/user avrg salary.dart
@@ -0,0 +1,95 @@
+void main() {
+  List <Map<String,dynamic>> users = [
+    {
+      "name" : "Sayed Hasan",
+      "age" : 23,
+      "email" : "sayeedhasan1387@gmail.com",
+      "jan" : 500,
+      "Mar" : 600,
+    },
+    
+    {
+      "name" : "karim hasan",
+      "age" : 25,
+      "email" : "karim123@gmail.com",
+      "jan" : 700,
+      "Mar" : 500,
+    },
+     {
+      "name" : "karim hasan",
+      "age" : 25,
+      "email" : "karim123@gmail.com",
+      "jan" : 700,
+      "Mar" : 500,
+    }, {
+      "name" : "karim hasan",
+      "age" : 25,
+      "email" : "karim123@gmail.com",
+      "jan" : 700,
+      "Mar" : 500,
+    },
+     {
+      "name" : "karim hasan",
+      "age" : 25,
+      "email" : "karim123@gmail.com",
+      "jan" : 700,
+      "Mar" : 500,
+    }, {
+      "name" : "karim hasan",
+      "age" : 25,
+      "email" : "karim123@gmail.com",
+      "jan" : 700,
+      "Mar" : 500,
+    }, {
+      "name" : "karim hasan",
+      "age" : 25,
+      "email" : "karim123@gmail.com",
+      "jan" : 700,
+      "Mar" : 500,
+    }, {
+      "name" : "karim hasan",
+      "age" : 25,
+      "email" : "karim123@gmail.com",
+      "jan" : 700,
+      "Mar" : 500,
+    }, {
+      "name" : "karim hasan",
+      "age" : 25,
+      "email" : "karim123@gmail.com",
+      "jan" : 700,
+      "Mar" : 500,
+    }, {
+      "name" : "karim hasan",
+      "age" : 25,
+      "email" : "karim123@gmail.com",
+      "jan" : 700,
+      "Mar" : 500,
+    }, {
+      "name" : "karim hasan",
+      "age" : 25,
+      "email" : "karim123@gmail.com",
+      "jan" : 700,
+      "Mar" : 500,
+    }, {
+      "name" : "karim hasan",
+      "age" : 25,
+      "email" : "karim123@gmail.com",
+      "jan" : 700,
+      "Mar" : 500,
+    }, {
+      "name" : "karim hasan",
+      "age" : 25,
+      "email" : "karim123@gmail.com",
+      "jan" : 700,
+      "Mar" : 500,
+    },
+    
+
+  ];
+  
+  
+  users.forEach(
+   (a) => print(" ${a['name']} - ${a['jan']+a['Mar']/2}") 
+  );
+  
+}
