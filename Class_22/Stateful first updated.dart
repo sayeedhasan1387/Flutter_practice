32b6165a79f8ecd25a8ed3a2ce1097a6763f5102
Sayeed Hasan <65469507+sayeedhasan1387@users.noreply.github.com>
2021-10-23 17:33:30 +0600

    Stateful first updated.dart
    creating function and passes

---
+++ Class_22/Stateful first updated.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyCounter());
+}
+
+class MyCounter extends StatefulWidget {
+  const MyCounter({Key? key}) : super(key: key);
+
+  @override
+  _MyCounterState createState() => _MyCounterState();
+}
+
+class _MyCounterState extends State<MyCounter> {
+  int _counter = 0;
+  void _decrement() {
+    () {
+      setState(() {
+        _counter--;
+      });
+      print("removed.$_counter");
+    };
+  }
+
+  void _restore() {
+    setState(
+      () {
+        _counter = 0;
+      },
+    );
+
+    print("Clicked");
+  }
+
+  void _increment() {
+    setState(
+      () {
+        _counter++;
+      },
+    );
+
+    print("Clicked");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        floatingActionButton: Container(
+          width: double.infinity,
+          child: Padding(
+            padding: const EdgeInsets.only(left: 30.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                FloatingActionButton(
+                  onPressed: _increment,
+                  child: Icon(Icons.remove),
+                ),
+                FloatingActionButton(
+                  onPressed: _restore,
+                  child: Icon(Icons.restore),
+                ),
+                FloatingActionButton(
+                  onPressed: _increment,
+                  child: Icon(Icons.add),
+                ),
+              ],
+            ),
+          ),
+        ),
+        appBar: AppBar(
+          title: Text("Counter"),
+        ),
+        body: Container(
+          height: double.infinity,
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text("You have to push buttom this many time"),
+              Text(
+                "$_counter",
+                style: TextStyle(
+                  fontSize: 44.0,
+                  color: Colors.grey,
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// class MyCounter extends StatefulWidget {
+//   const MyCounter({Key? key}) : super(key: key);
+
+
+
+
+
